a8090445841747990b3d32aea2a71aa284ec1fbc
Jaleelgz <abduljaleelkolamban@gmail.com>
2023-08-15 11:23:17 +0530

    Cors bug fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
