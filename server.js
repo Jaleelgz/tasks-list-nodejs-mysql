ea3fe749cad750116340c61166eac0ab0c2b8c3d
Jaleelgz <abduljaleelkolamban@gmail.com>
2023-08-15 11:40:34 +0530

    cors methods

--- server.js
+++ server.js
@@ -9,12 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "HEAD"],
-  })
-);
+app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
